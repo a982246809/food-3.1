d453d552d030e85aacc1077a6fde327db697acb6
a982246809 <982246809@qq.com>
2026-02-28 21:53:01 +0800

    feat: 实现认证服务，包含用户验证、登录、注册功能及JWT策略。

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -21,7 +21,7 @@ export class AuthService {
   }
 
   async login(user: any) {
-    const payload = { username: user.username, sub: user.id, role: user.role };
+    const payload = { username: user.username, sub: user.id, role: user.role, windowId: user.windowId, canteenId: user.canteenId };
     return {
       access_token: this.jwtService.sign(payload),
       user: {
