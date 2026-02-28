beb4a4ec9173626547579e5cb6415f10d2276d67
a982246809 <982246809@qq.com>
2026-02-28 20:21:08 +0800

    2

--- web-client/src/api/request.ts
+++ web-client/src/api/request.ts
@@ -21,7 +21,17 @@ request.interceptors.request.use(
 
 request.interceptors.response.use(
   (response) => {
-    return response.data;
+    const res = response.data;
+    // Unwrap our standard backend response
+    if (res && res.code) {
+      if (res.code === '0000') {
+        return res.data;
+      } else {
+        showNotify({ type: 'danger', message: res.msg || '请求失败，请稍后再试' });
+        return Promise.reject(new Error(res.msg || 'Error'));
+      }
+    }
+    return res;
   },
   (error) => {
     const errorMsg = error.response?.data?.message || '请求失败，请稍后再试';
